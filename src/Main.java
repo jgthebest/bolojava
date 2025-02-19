dc1dc81c237cb0370e49f6af8c62b373da2b2263
Laboratorio <laboratorio@uniprojecao.edu.br>
2025-02-19 10:04:51 -0300

    a

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criando o objeto Bolo (bolo pronto)
+        Bolo bolo = new Bolo("Chocolate", 20);
+        bolo.exibirDetalhes();
+    }
+}
